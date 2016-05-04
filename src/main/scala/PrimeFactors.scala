b05b9bbc70d354294b8805854a68f279e45605ab
mac.grewal <mac.grewal@digital.hmrc.gov.uk>
2016-05-04 11:06:35 +0100

    Credit for prime factors implementation included

--- src/main/scala/PrimeFactors.scala
+++ src/main/scala/PrimeFactors.scala
@@ -1,6 +1,7 @@
 package primeFactors
 
 object PrimeFactors {
+  // credit for implementation goes to kabuumu
   def result(n: Int, l: List[Int] = Nil): List[Int] = {
     (2 to n)
       .find(n % _ == 0)
