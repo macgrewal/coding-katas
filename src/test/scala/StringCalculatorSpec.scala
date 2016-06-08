17f96f32359a25000463c4b8eecbacc644b126bf
mac.grewal <mac.grewal@digital.hmrc.gov.uk>
2016-06-08 11:04:58 +0100

    End of string calculator - session 1

---
+++ src/test/scala/StringCalculatorSpec.scala
@@ -0,0 +1,52 @@
+import org.scalatest.FlatSpec
+
+class StringCalculatorSpec extends FlatSpec {
+
+  "Calling Add with and empty string" should "return 0" in {
+    assertResult(0) {
+      StringCalculator.add("")
+    }
+  }
+
+  "Calling Add with 1" should "return 1" in {
+    assertResult(1) {
+      StringCalculator.add("1")
+    }
+  }
+
+  "Calling Add with 2" should "return 2" in {
+    assertResult(2) {
+      StringCalculator.add("2")
+    }
+  }
+
+  "Calling Add with 1,1" should "return 2" in {
+    assertResult(2) {
+      StringCalculator.add("1,1")
+    }
+  }
+
+  "Calling Add with 1,2" should "return 3" in {
+    assertResult(3) {
+      StringCalculator.add("1,2")
+    }
+  }
+
+  "Calling Add with 1,2,3,0,4" should "return 10" in {
+    assertResult(10) {
+      StringCalculator.add("1,2,3,0,4")
+    }
+  }
+
+  "Calling Add with 1\\n1" should "return 2" in {
+    assertResult(2) {
+      StringCalculator.add("1\n1")
+    }
+  }
+
+  "Calling Add with 1\\n1,2" should "return 4" in {
+    assertResult(4) {
+      StringCalculator.add("1\n1,2")
+    }
+  }
+}
